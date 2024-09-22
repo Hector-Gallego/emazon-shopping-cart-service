8d824c90b2ad9a4b46daae9c5dd8358dde5f0f5e
Hector Gallego <gallegohector96@gmail.com>
2024-09-22 08:57:38 -0500

    initial configuration project

---
+++ src/main/java/com/resourceserver/emazonshoppingcartservice/EmazonShoppingCartServiceApplication.java
@@ -0,0 +1,13 @@
+package com.resourceserver.emazonshoppingcartservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmazonShoppingCartServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmazonShoppingCartServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/resourceserver/emazonshoppingcartservice/EmazonShoppingCartServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.resourceserver.emazonshoppingcartservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmazonShoppingCartServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
